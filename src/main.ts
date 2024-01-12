a52986ff39aaa211c5018c476fe51ef10d9cfa55
wjkang <ruoxieme@gmail.com>
2024-01-12 23:53:20 +0800

    ✨ feat: webview

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import "uno.css";
 import "./style.scss";
 import App from "./App.vue";
-
+import { init } from "./webview";
+init();
 createApp(App).mount("#root");
